54b848e4821bf439c4c740ceec562205824a77f8
Lars de Klijne <larsdeklijne@gmail.com>
2020-08-26 17:17:41 +0100

    routes aangemaakt voor het authenticatiesysteem op basis van tokens

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Authenitcation routes
+Route::group(['prefix' => 'api'], function()
+{
+    Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
+
+    // route:: localhost/api/authenticate
+    Route::post('authenticate', 'AuthenticateController@authenticate');
+});
+
+Route::get('/checkIfAuthenticated', 'AuthenticateController@checkIfAuthenticated')->middleware('checkToken');
 
 // medicine routes
 Route::get('/medicine/index', 'medicineController@index');
