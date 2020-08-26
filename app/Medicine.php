b643b179b741b85f17e585fbe4f43717edd5b55a
Lars de Klijne <larsdeklijne@gmail.com>
2020-08-26 16:25:42 +0100

    medicine model geconfigureerd met database velden

--- app/Medicine.php
+++ app/Medicine.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medicine extends Model
 {
-    //
+    protected $table = 'medicines';
+
+    protected $fillable = ['naam', 'dosering', 'wanneer', 'datum_van', 'datum_tot', 'tijd'];
 }
